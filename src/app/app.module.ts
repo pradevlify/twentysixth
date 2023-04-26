22f2fcf74edc4a3c7293c07a712c2d3090b214f8
prathamesh kambli <prathameshkambli8@gmail.com>
2023-04-26 18:13:50 +0530

    login commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NcComponent } from './nc/nc.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NcComponent
+    NcComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
